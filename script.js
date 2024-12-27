8f641c78d4aa7a8c1b5f093cc42f2a0bf96a3401
Andy Salerno <andysalerno@gmail.com>
2024-12-26 22:02:47 -0800

    remove unneeded stuff

--- script.js
+++ script.js
@@ -9,31 +9,12 @@ async function fetchCards() {
     const cards = lines.map(line => JSON.parse(line));
 
     return cards;
-
-    // This would be replaced with your actual API endpoint
-    // return [
-    //     {
-    //         card_uuid: "d421208c-2f93-54aa-b8e8-ad42fe26c505",
-    //         card_name: "Kaito, Cunning Infiltrator",
-    //         card_text: "Whenever a creature you control deals combat damage to a player, put a loyalty counter on Kaito.\n[+1]: Up to one target creature you control can't be blocked this turn. Draw a card, then discard a card.\n[−2]: Create a 2/1 blue Ninja creature token.\n[−9]: You get an emblem with \"Whenever a player casts a spell, you create a 2/1 blue Ninja creature token.\"",
-    //         output: {
-    //             effects: [
-    //                 { event: "addCounter", who: "you", immediate: false },
-    //                 { event: "draw", who: "you", immediate: false },
-    //                 { event: "discard", who: "you", immediate: false },
-    //                 { event: "createToken", who: "you", immediate: false }
-    //             ]
-    //         }
-    //     }
-    //     // More cards would be here
-    // ];
 }
 
 function createCardElement(card) {
     const cardDiv = document.createElement('div');
     cardDiv.className = 'card';
 
-    // https://cards.scryfall.io/normal/front/d/e/de1c8758-ce3d-49cf-8173-c0eb46f5e7bc.jpg
     const img = document.createElement('img');
     img.className = 'card-image';
     const scryfall_id = card.scryfall_id;
@@ -70,12 +51,10 @@ function lazyLoad() {
             if (entry.isIntersecting) {
                 const img = entry.target;
                 img.src = img.dataset.src;
-                // Stop observing the image after loading
                 observer.unobserve(img);
             }
         });
     }, {
-        // Start loading when image is 50px away from viewport
         rootMargin: '50px'
     });
 
